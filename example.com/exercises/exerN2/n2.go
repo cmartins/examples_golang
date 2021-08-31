071a6c17136991982d24b0c5d07ed361e286b385
Cristiano Martins <cristiano.cms@gmail.com>
2021-08-31 18:34:24 -0300

    add exercises n2

--- example.com/exercises/exerN2/n2.go
+++ example.com/exercises/exerN2/n2.go
@@ -4,6 +4,16 @@ import (
 	"fmt"
 )
 
+const exe03_x int = 10
+const exe03_y = 10
+const (
+	_ = 2021 + iota
+	exe06_b
+	exe06_c
+	exe06_d
+	exe06_e
+)
+
 func exer01() {
 	x := 100
 	fmt.Printf("%d, %#x, %b", x, x, x)
@@ -17,11 +27,45 @@ func exer02() {
 	e := (10 >= 100)
 	f := (10 > 100)
 
-	fmt.Printf("%v\n%v\n%v\n%v\n%v\n%v\n", a, b, c, d, e, f)
+	fmt.Printf("\n%v\n%v\n%v\n%v\n%v\n%v\n", a, b, c, d, e, f)
+}
 
+func exer03() {
+	fmt.Printf("\nx: %v - y: %v", exe03_x, exe03_y)
+}
+
+func exer04() {
+	x := 300
+	fmt.Printf("\n%d\t%b\t%#x\n", x, x, x)
+	y := x << 1
+	fmt.Printf("%d\t%b\t%#x", y, y, y)
+}
+
+func exer05() {
+	x := `
+		Algum texto
+					com 
+
+					formatacao
+	`
+	fmt.Println(x)
+}
+
+func exer06() {
+	fmt.Println(exe06_b, exe06_c, exe06_d, exe06_e)
 }
 
 func main() {
+	fmt.Println("######## Exercise 01 ########")
 	exer01()
+	fmt.Println("\n\n######## Exercise 02 ########")
 	exer02()
+	fmt.Println("\n\n######## Exercise 03 ########")
+	exer03()
+	fmt.Println("\n\n######## Exercise 04 ########")
+	exer04()
+	fmt.Println("\n\n######## Exercise 05 ########")
+	exer05()
+	fmt.Println("\n\n######## Exercise 06 ########")
+	exer06()
 }
